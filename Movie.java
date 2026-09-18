d5176a4287ebf10eb4944aa5908a599b5fd2e49f
jeminaamores-ctrl <jeminaamores@gmail.com>
2026-09-17 17:58:08 -0700

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,15 @@
+
+
+public class Movie{
+
+     
+   String title;
+   String genre;
+   int duration;
+   
+   void displayInfo(){
+   System.out.println("Movie: " + title + " " + genre + " "  + duration + " minutes");
+   
+   
+}
+}
